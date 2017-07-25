add275ece861b89747d81020205f2833c269b697
xuqian <xuqian@tenorsharedeiMac.local>
2017-07-25 18:49:36 +0800

    添加新的类

---
+++ MutlThreadPhotos/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  MutlThreadPhotos
+//
+//  Created by xuqian on 2017/7/25.
+//  Copyright © 2017年 xuqian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ MutlThreadPhotos/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  MutlThreadPhotos
+//
+//  Created by xuqian on 2017/7/25.
+//  Copyright © 2017年 xuqian. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
